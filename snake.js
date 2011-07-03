60d487ba97d7697c9069253df2877324171145a6
GaoLi <3071730@qq.com>
2011-07-03 19:29:27 +0800

    update for snake move

--- snake.js
+++ snake.js
@@ -1,27 +1,148 @@
-function snake() {
-	this.init();
+function snake(row, column, len) {
+	this.row = row;
+	this.column = column;
+	this.len = len;
+	this.init(this.row, this.column, this.len);
 };
 
 //init
-snake.prototype.init = function() {
-	this.initGrid(20, 20);
-}
+snake.prototype.init = function(row, column, len) {
+	var self = this;
+
+	self.initGrid(row, column);
+	self.initSnake(column, len);
+	self.start();
+};
+
+//start
+snake.prototype.start = function() {
+	var self = this;
+	keyCode = 39;
+
+	window.onkeydown = self.attachEvents;
+	setInterval(function() {
+		self.step(self);
+	},
+	500);
+};
+
+//attachEvents
+snake.prototype.attachEvents = function(e) {
+	var self = this,
+	e = e || event,
+	key = e.which || e.keyCode;
+
+	if (key >= 37 && key <= 40) {
+		keyCode = key;
+	} else {
+		return false;
+	}
+};
 
 //initGrid
 snake.prototype.initGrid = function(row, column) {
-	var snake = document.getElementById("snake");
-	html = "";
+	var self = this,
+	wrap = document.getElementById("wrap"),
+	grid = self.multiArray(row, column),
+	tr,
+	td;
 
-	for (var i = 1; i < row; i++) {
-		html += "<tr>";
-		for (var j = 1; j < column; j++) {
-			html += "<td></td>";
+	self.state = self.multiArray(row, column);
+
+	for (var i = 0; i < row; i++) {
+		tr = document.createElement("tr");
+		for (var j = 0; j < column; j++) {
+			td = document.createElement("td");
+			grid[j][i] = tr.appendChild(td);
 		}
-		html += "</td>";
+		wrap.appendChild(tr);
 	}
 
-	snake.innerHTML = html;
-
+	self.grid = grid;
 };
 
-new snake();
+//initSnake
+snake.prototype.initSnake = function(column, len) {
+	var self = this,
+	p = self.random(len - 1),
+	x = p[0],
+	y = p[1];
+
+	self.body = new Array();
+
+	for (i = 0; i < len; i++) {
+		self.body.push([x, y]);
+		self.state[x][y] = true;
+		self.grid[x][y].className = "cover";
+
+		x = x - 1;
+	}
+};
+
+//step
+snake.prototype.step = function(self) {
+	var self = this,
+	body = self.body,
+	head = body[0],
+	foot = body.pop(),
+	state = self.state,
+	grid = self.grid,
+	hx = head[0],
+	hy = head[1],
+	fx = foot[0],
+	fy = foot[1];
+
+	console.log(keyCode);
+
+	switch (keyCode) {
+	case 37:
+		hx -= 1;
+		break;
+	case 38:
+		hy -= 1;
+		break;
+	case 39:
+		hx += 1;
+		break;
+	case 40:
+		hy += 1;
+		break;
+	}
+
+	state[fx][fy] = false;
+	grid[fx][fy].className = "";
+	body.unshift([hx, hy]);
+	state[hx][hy] = true;
+	grid[hx][hy].className = "cover";
+};
+
+//random
+snake.prototype.random = function(sx, sy, ex, ey) {
+	var self = this,
+	temp = [],
+	x,
+	y;
+
+	sx = sx || 0;
+	sy = sy || 0;
+	ex = ex || self.column;
+	ey = ey || self.row;
+
+	x = Math.floor(Math.random() * (ex - sx)) + sx;
+	y = Math.floor(Math.random() * (ey - sy)) + sy;
+
+	temp[0] = x;
+	temp[1] = y;
+	return temp;
+};
+
+//multiArray
+snake.prototype.multiArray = function(m, n) {
+	var arr = new Array(n);
+	for (var i = 0; i < m; i++) {
+		arr[i] = new Array(n);
+	}
+	return arr;
+}
+
+new snake(20, 20, 3);
