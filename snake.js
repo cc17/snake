a96b08bb17487a3e35128ef5eee2b25d5d8c8594
gaoli <3071730@qq.com>
2012-02-08 17:45:34 +0800

    update for snake move

--- snake.js
+++ snake.js
@@ -1,114 +1,187 @@
-// Model
-var Box = Backbone.Model.extend({
+(function(win) {
 
-    defaults: {
-        snake: false,
-        food: false
-    }
+	var SNAKESMOVE;
 
-});
+	// Model
+	var Box = Backbone.Model.extend({
+		defaults: {
+			state: false
+		}
+	}),
 
-// Collection
-var Boxs = Backbone.Collection.extend({
+	Snake = Backbone.Model.extend({
 
-    model: Box,
-    x: 20,
-    y: 20,
-    l: 5
+	});
 
-});
+	// Collection
+	var Boxs = Backbone.Collection.extend({
+		model: Box,
+		x: 20,
+		y: 20,
+		k: 39
+	}),
 
-var BOXS = new Boxs;
+	Snakes = Backbone.Collection.extend({
+		model: Snake,
+		l: 5,
+		s: 500
+	}),
 
-// View
-var BoxView = Backbone.View.extend({
+	BOXS = new Boxs;
 
-    tagName: "td",
+	SNAKES = new Snakes;
 
-    initialize: function() {
-        this.model.bind("change:snake", this.render, this);
-    },
+	// View
+	var BoxView = Backbone.View.extend({
 
-    render: function() {
-        $(this.el).addClass("snake");
-        return this;
-    }
+		tagName: "td",
 
-});
+		initialize: function() {
+			this.model.bind("change:state", this.render, this);
+		},
 
-var BoxsView = Backbone.View.extend({
+		render: function() {
 
-    el: "#wrap",
+			switch (this.model.get("state")) {
+			case true:
+				$(this.el).addClass("snake");
+				break;
+			case false:
+				$(this.el).removeClass("snake");
+				break;
+			}
 
-    initialize: function() {
-        this.render();
-        this.initSnake();
-    },
+			return this;
+		}
 
-    render: function() {
+	}),
 
-        for (i = BOXS.y; i--;) {
-            var tr = $("<tr></tr>");
+	BoxsView = Backbone.View.extend({
 
-            for (j = BOXS.x; j--;) {
-                var id = j + "-" + i;
+		el: "#wrap",
 
-                BOXS.add({
-                    "id": id 
-                });
+		initialize: function() {
+			this.render();
+		},
 
-                var view = new BoxView({
-                    "model": BOXS.get(id)
-                });
+		render: function() {
 
-                tr.append(view.el);
-            }
+			for (i = 0; i < BOXS.y; i++) {
+				var tr = $("<tr></tr>");
 
-            $(this.el).append(tr);
-        }
+				for (j = 0; j < BOXS.x; j++) {
+					var mID = j + "-" + i;
 
-    },
+					BOXS.add({
+						id: mID
+					});
 
-    initSnake: function() {
-        
-        var self = this,
-            p = self.random(BOXS.l - 1);
-        
-        for (i = 0; i < BOXS.l; i++) {
-            self.setSnake(p.x - i, p.y, true);
-        }
+					var view = new BoxView({
+						model: BOXS.get(mID)
+					});
 
-    },
+					tr.append(view.el);
+				}
 
-    random: function(sx, sy, ex, ey) {
-        var self = this,
-        x,
-        y;
+				$(this.el).append(tr);
+			}
 
-        sx = sx || 0;
-        sy = sy || 0;
-        ex = ex || BOXS.x;
-        ey = ey || BOXS.y;
+			this.initSnake();
+			this.move();
 
-        x = Math.floor(Math.random() * (ex - sx)) + sx;
-        y = Math.floor(Math.random() * (ey - sy)) + sy;
+		},
 
-        if (self.getSnake(x, y)) {
-            return self.random(sx, sy, ex, ey);
-        }
+		initSnake: function() {
+			var self = this,
+			p = self.random(SNAKES.l - 1);
 
-        return {"x": x, "y": y};
-    },
+			for (i = 0; i < SNAKES.l; i++) {
+				self.setSnake(p.x + i, p.y, true);
+			}
+		},
 
-    getSnake: function(x, y) {
-        return BOXS.get(x + "-" + y).get("snake");         
-    },
+		move: function(s) {
+			var self = this;
 
-    setSnake: function(x, y, val) {
-        BOXS.get(x + "-" + y).set({"snake": val});
-    }
+			(s) ? win.clearInterval(snakeMove) : s = SNAKES.s;
 
-});
+			snakeMove = win.setInterval(function() {
+				self.step();
+			},
+			s);
+		},
 
-new BoxsView;
+		step: function() {
+			var l = SNAKES.length - 1,
+			m = SNAKES.models,
+			hx = m[l].get("x"),
+			hy = m[l].get("y"),
+			fx = m[0].get("x"),
+			fy = m[0].get("y");
+
+			switch (BOXS.k) {
+			case 37:
+				hx === 0 ? hx = column - 1: hx -= 1;
+				break;
+			case 38:
+				hy === 0 ? hy = row - 1: hy -= 1;
+				break;
+			case 39:
+				hx === BOXS.x - 1 ? hx = 0: hx += 1;
+				break;
+			case 40:
+				hy === BOXS.y - 1 ? hy = 0: hy += 1;
+				break;
+			}
+
+			this.setSnake(hx, hy, true);
+			this.setSnake(fx, fy, false);
+		},
+
+		random: function(sx, sy, ex, ey) {
+			var self = this,
+			x, y;
+
+			sx = sx || 0;
+			sy = sy || 0;
+			ex = ex || BOXS.x;
+			ey = ey || BOXS.y;
+
+			x = Math.floor(Math.random() * (ex - sx));
+			y = Math.floor(Math.random() * (ey - sy));
+
+			if (self.getSnake(x, y)) {
+				return self.random(sx, sy, ex, ey);
+			}
+
+			return {
+				"x": x,
+				"y": y
+			};
+		},
+
+		getSnake: function(x, y) {
+			return BOXS.get(x + "-" + y).get("state");
+		},
+
+		setSnake: function(x, y, val) {
+			BOXS.get(x + "-" + y).set({
+				state: val
+			});
+
+			if (val) {
+				SNAKES.add({
+					"x": x,
+					"y": y
+				});
+			} else {
+				SNAKES.remove((SNAKES.models)[0]);
+			}
+		}
+
+	});
+
+	new BoxsView;
+
+})(window)
 
